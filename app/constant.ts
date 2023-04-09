308207f3605ca77c071e65a3a7f9717282d8335b
gufei6 <83202936+gufei6@users.noreply.github.com>
2023-04-10 01:00:45 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,4 +1,4 @@
-export const OWNER = "gufei";
+export const OWNER = "gufei6";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${gufei6}/${REPO}`;
 export const ISSUE_URL = `https://github.com/${gufei6}/${REPO}/issues`;
