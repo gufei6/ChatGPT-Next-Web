9f1b27123b6c4bfbb37a495008577efd6d1e75d4
gufei6 <83202936+gufei6@users.noreply.github.com>
2023-04-10 14:16:27 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
-export const OWNER = "77i";
+export const OWNER = "baidu";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const REPO_URL = `https://baidu.com/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://baidu.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const FETCH_COMMIT_URL = `https://api.baidu.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
+export const FETCH_TAG_URL = `https://api.baidu.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
